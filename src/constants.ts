bb2ad03a767dc9cc407e0bce8f7ef0383837e70d
SpectreSwap <spectreswapftm@protonmail.com>
2021-05-28 18:16:54 +0300

    feat: edit router

--- src/constants.ts
+++ src/constants.ts
@@ -19,9 +19,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const FACTORY_ADDRESS = '0xa9d1df5f796903B0b3B8752C328850c6fde50ace'
 
-export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+export const INIT_CODE_HASH = '0x33dd987a57b493b0046a1e62122aeaf567b1d121e3b1833dc883b6f3f2a6f3ec'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
